841b72edbfe372ef8aec35a013c500ef47478fba
Danilo Pereira <danilo4web@gmail.com>
2018-02-26 11:16:22 -0300

    #PHP #Laravel - Util routes

---
+++ php.utils/Util.php
@@ -0,0 +1,55 @@
+<?php
+class Util {
+	
+	/**
+	 * converte data d/m/Y para Y-m-d
+	 *
+	 * @param string $data
+	 * @return string
+	 */
+	public function dateToDb($data) {
+		return !empty($data) ? implode("-", array_reverse(explode("/", $data))) : NULL;
+	}
+	
+	/**
+	 * converte data Y-m-d para d/m/Y (ou d/m/Y H:i:s)
+	 *
+	 * @param string $data
+	 * @return string
+	 */
+	public function dbToDate($data, $with_hours = 0) {
+		
+		if($with_hours) {
+			$hour = substr($data, 11, 8);
+			$data = substr($data, 0, 10);
+		}
+		
+		return (!empty($data) ? implode("/", array_reverse(explode("-", $data))) : '') . (isset($hour) ? ' ' . $hour : '');
+	}
+	
+	/**
+	 * converte string para camelCase
+	 * @param string $str
+	 * @param boolean $first_UPPERCASE
+	 * @return string
+	 */
+	public function convertCamelCase($str, $first_UPPERCASE = false) {
+		if($first_UPPERCASE) {
+			$str[0] = strtoupper($str[0]);
+		}
+		
+		return preg_replace_callback('/_([a-z])/', create_function('$c', 'return strtoupper($c[1]);'), $str);
+	}
+	
+	/**
+	 * converte CamelCase para string_without_camel_case
+	 * @param string $str
+	 * @return string
+	 */
+	public function fromCamelCase($str) {
+		$str[0] = strtolower($str[0]);
+		$func = create_function('$c', 'return "_" . strtolower($c[1]);');
+		return preg_replace_callback('/([A-Z])/', $func, $str);
+	}
+	
+}
